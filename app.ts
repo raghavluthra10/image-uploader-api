ec935d988163d6463a9f4ce272bd0fac72c8f8c2
Raghav Luthra <raghavluthra1@gmail.com>
2022-11-29 21:59:51 +0530

    setup

---
+++ app.ts
@@ -0,0 +1,6 @@
+function myName(name: string): string {
+   return "my name is " + name;
+}
+
+const name1 = myName("raghav");
+console.log(name1);
---
+++ dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+function myName(name) {
+    return "my name is " + name;
+}
+const name1 = myName("raghav");
+console.log(name1);
+//# sourceMappingURL=app.js.map
