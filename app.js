1ac5b250bec6f621b52cd89b3b232c7d5355f8cb
Raghav Luthra <raghavluthra1@gmail.com>
2022-11-30 00:14:07 +0530

    server initial setup

---
+++ app.js
@@ -0,0 +1,5 @@
+function myName(name) {
+    return "my name is " + name;
+}
+var name1 = myName("raghav");
+console.log(name1);
--- app.ts
+++ app.ts
@@ -1,6 +1,11 @@
-function myName(name: string): string {
-   return "my name is " + name;
-}
+import express, { Request, Response, NextFunction } from "express";
+const app = express();
+const port = 8000;
 
-const name1 = myName("raghav");
-console.log(name1);
+app.get("/", (req: Request, res: Response) => {
+   res.send("Hello World!");
+});
+
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`);
+});
