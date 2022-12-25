057da2ef0cf732ad3456758012d931aef6f4c541
Raghav Luthra <raghavluthra1@gmail.com>
2022-12-25 18:47:57 +0530

    served client bundle by express server

--- app.ts
+++ app.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import cors from "cors";
+// import cors from "cors";
 import cookieParser from "cookie-parser";
 import { router } from "./routes/index";
 import { database } from "./config/database";
 import { User } from "./interfaces/databaseTables";
 import addHeaders from "./middleware/cors";
+import path from "path";
 
 dotenv.config({ path: "./env" });
 
@@ -16,8 +17,7 @@ const port = 8000;
 const checkDbConnection = async () => {
   try {
     const response = await database("user");
-    console.log("response ->", response);
-    if (response.length > 0) {
+    if (response.length >= 0) {
       console.log("database =>", "connected Successfully!");
     }
   } catch (error) {
@@ -28,6 +28,7 @@ const checkDbConnection = async () => {
 
 checkDbConnection();
 
+app.use(express.static(path.join(__dirname + "/client/dist")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -38,16 +39,6 @@ app.use(cookieParser());
 app.use(addHeaders);
 
 app.use("/", router);
-
-app.get("/", async (req: Request, res: Response) => {
-  try {
-    const getUser: User[] = await database("user").where({ id: 1 });
-    console.log(getUser[0]);
-    res.send("Hello World! =>" + JSON.stringify(getUser[0]));
-  } catch (error: unknown) {
-    console.log(error);
-  }
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port} `);
