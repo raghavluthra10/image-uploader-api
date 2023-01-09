285a6997af535e9650ea60b1da8ce84b4cee4ec5
Raghav Luthra <raghavluthra1@gmail.com>
2023-01-10 00:22:54 +0530

    redirect to home if already logged in

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -50,6 +50,17 @@ interface IAppProps {
 }
 
 export default function App({ setUserAuth }: IAppProps) {
+  const navigator = useNavigate();
+  React.useEffect(() => {
+    const token = window.localStorage.getItem("Authenticate");
+
+    // if user already logged it then redirect to home
+    if (token) {
+      navigator("/home");
+      return;
+    }
+  }, []);
+
   const [loginForm, setLoginForm] = React.useState<LoginForm>({
     email: "",
     password: "",
