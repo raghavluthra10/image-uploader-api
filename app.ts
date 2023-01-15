823ce56aaa49d5d7c92a82a648700bf34b0553f1
Raghav Luthra <raghavluthra1@gmail.com>
2023-01-15 20:46:20 +0530

    docker-compose.yml for starting server and db

--- app.ts
+++ app.ts
@@ -13,19 +13,19 @@ dotenv.config({ path: "./env" });
 const app = express();
 const port = 8000;
 
-const checkDbConnection = async () => {
-  try {
-    const response = await database("user");
-    if (response.length >= 0) {
-      console.log("database =>", "connected Successfully!");
-    }
-  } catch (error) {
-    console.log("Some error occured in database connection");
-    console.log(error);
-  }
-};
+// const checkDbConnection = async () => {
+//   try {
+//     const response = await database("user");
+//     if (response.length >= 0) {
+//       console.log("database =>", "connected Successfully!");
+//     }
+//   } catch (error) {
+//     console.log("Some error occured in database connection");
+//     console.log(error);
+//   }
+// };
 
-checkDbConnection();
+// checkDbConnection();
 
 app.use(express.static(path.join(__dirname + "/client/dist")));
 
