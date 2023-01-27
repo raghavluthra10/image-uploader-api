28685c0be7b3f1c24d574339b62c08b5098ddda0
Raghav Luthra <raghavluthra1@gmail.com>
2023-01-27 20:38:23 +0530

    changed dev env to prod

--- client/src/axios.config.ts
+++ client/src/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // development
-export default axios.defaults.baseURL = `http://localhost:8000/api`;
+// export default axios.defaults.baseURL = `http://localhost:8000/api`;
 
 // production
-// export default axios.defaults.baseURL = `${window.location.origin}/api`;
+export default axios.defaults.baseURL = `${window.location.origin}/api`;
